2c42b01092df0651c1a4de150427e71998b1427a
Polina Simonenko <rabarbrablad@gmail.com>
2026-06-20 16:46:47 +0200

    bump v0.1.7
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- Formula/exex.rb
+++ Formula/exex.rb
@@ -1,28 +1,28 @@
 class Exex < Formula
   desc "Terminal UI for exploring ELF, Mach-O and PE binaries"
   homepage "https://github.com/rabarbra/exex"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/rabarbra/exex/releases/download/v#{version}/exex-v#{version}-darwin-arm64.tar.gz"
-      sha256 "fa8c94870fabff86ac9ed709f2725fcd178fe1ec653c431286eb1f55a549da08"
+      sha256 "cd3918893e3f0cb0da071e8ce798adacfb30c81d1dfe981760d5d5cf1aac24e4"
     end
     on_intel do
       url "https://github.com/rabarbra/exex/releases/download/v#{version}/exex-v#{version}-darwin-amd64.tar.gz"
-      sha256 "b8737a30dd831301dd598f5c1f105347a954a752fbfafedc20660be46eb01e61"
+      sha256 "1cf507eff3286499be4381df1f53ae0f8554bf49ef54c6d6d4db1e1aadc82f8e"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/rabarbra/exex/releases/download/v#{version}/exex-v#{version}-linux-arm64.tar.gz"
-      sha256 "e2560cb0cae9b8f935b8a4611f8340a32cd49248ecc4e327e5f499383d6f6386"
+      sha256 "e593cb79bb2ae599d1e2290ed7ebae3028ca03969b4b2fe40bfd5ccec0cdfb21"
     end
     on_intel do
       url "https://github.com/rabarbra/exex/releases/download/v#{version}/exex-v#{version}-linux-amd64.tar.gz"
-      sha256 "6e808ddb2fc3e11976045c9cd208c7b48b220f38f0a706664075003369f785b9"
+      sha256 "84891f03b8a149a3e67d3ae20737738af986838fe6367a1a1da22b793f8c7f7c"
     end
   end
 
@@ -30,6 +30,12 @@ class Exex < Formula
     bin.install "exex"
     pkgshare.install "config.example.yaml" if File.exist?("config.example.yaml")
     doc.install "README.md" if File.exist?("README.md")
+
+    man1.install "exex.1" if File.exist?("exex.1")
+
+    bash_completion.install "completions/exex.bash" => "exex" if File.exist?("completions/exex.bash")
+    zsh_completion.install "completions/_exex" if File.exist?("completions/_exex")
+    fish_completion.install "completions/exex.fish" if File.exist?("completions/exex.fish")
   end
 
   test do
